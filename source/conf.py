0da45148756ece9ca3494611a5ef830de591ccf5
Takahito Urai <uraitakahito@gmail.com>
2023-01-22 20:07:39 +0900

    change the html_theme

--- source/conf.py
+++ source/conf.py
@@ -23,5 +23,5 @@ language = 'en'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'scrolls'
 html_static_path = ['_static']
